dd86bdd7d8b75cc86b07ff30fa9f8ae852632378
LukReis <lucasesdras8@gmail.com>
2025-08-31 14:57:48 -0300

    Entendendo Sobre Escopo de Variaveis

---
+++ src/IniciandoComALinguagem/EscopoVariaveis.java
@@ -0,0 +1,18 @@
+package IniciandoComALinguagem;
+
+public class EscopoVariaveis {
+    public static void main(String[] args) {
+
+
+
+
+        if (false) {
+            double salario = 100.8;
+            System.out.println(salario);
+        } else {
+
+        }
+
+        System.out.println("Testando Execução");
+    }
+}
