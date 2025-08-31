30e7743d8e9c79f92a942a4d008d00681b0fe012
LukReis <lucasesdras8@gmail.com>
2025-08-31 12:38:29 -0300

    Aprendendo Sobre Casting

---
+++ src/IniciandoComALinguagem/Casting.java
@@ -0,0 +1,12 @@
+package IniciandoComALinguagem;
+
+public class Casting {
+    public static void main(String[] args) {
+        double salarioAnual = 98275.77;
+
+        int valor = (int)salarioAnual;
+
+        System.out.println(salarioAnual);
+        System.out.println(valor);
+    }
+}
