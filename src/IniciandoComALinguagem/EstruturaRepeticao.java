baabb8c322fea1c5ef157139dced5b5512998cd5
LukReis <lucasesdras8@gmail.com>
2025-08-31 14:02:41 -0300

    Aprendendo Sobre Repetições com While

---
+++ src/IniciandoComALinguagem/EstruturaRepeticao.java
@@ -0,0 +1,14 @@
+package IniciandoComALinguagem;
+
+public class EstruturaRepeticao {
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        while(contador <= 5) {
+            System.out.println(contador);
+            contador++;
+        }
+
+    }
+}
